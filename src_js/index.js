32c71f6efa23bb8ea7c83b9152dfbf9ce4124d2b
Suraiya Hameed <v-suhame@microsoft.com>
2017-10-27 15:20:06 -0700

    refactor: handle handshake
    native.js contains changes made by @arthurschreiber that make sspi-client pluggable

--- src_js/index.js
+++ src_js/index.js
@@ -1,17 +1,18 @@
-'use strict';
-
 var os = require('os');
 
-// require and export only for platforms where the module is supported.
-// Individual module require'ed use features of node.js that would trigger
-// syntax errors in older versions. This allows for the module to be included
-// in applications like Tedious which supports older version of node.js, even
-// if the functionality itself won't be available. The application can decide
-// what to do if the module is not supported on the platform where it's running.
-if (require('semver').gte(process.version, '4.0.0') && os.type() === 'Windows_NT') {
-  module.exports.ModuleSupported = true;
+const NativeAuthProvider = require('./native.js').NativeAuthProvider;
 
-  module.exports.SspiClientApi = require('./sspi_client');
-  module.exports.Fqdn = require('./fqdn');
-  module.exports.MakeSpn = require('./make_spn');
-}
+module.exports = function(options) {
+  // require and export only for platforms where the module is supported.
+  // Individual module require'ed use features of node.js that would trigger
+  // syntax errors in older versions. This allows for the module to be included
+  // in applications like Tedious which supports older version of node.js, even
+  // if the functionality itself won't be available. The application can decide
+  // what to do if the module is not supported on the platform where it's running.
+  if (require('semver').gte(process.version, '4.0.0') && os.type() === 'Windows_NT') {
+    return function(connection) {
+      return new NativeAuthProvider(connection, options);
+    };
+  }
+  return undefined;
+};
